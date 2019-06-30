bfa6f067ba7f254386e4a17763d64135ace216e8
Sam Bogoslavets <sam_1997@mail.ru>
2019-06-30 20:12:35 +0300

    Update SCC2.c

--- SCC2.c
+++ SCC2.c
@@ -1,55 +1,35 @@
-
-#include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
- 
-#define TEST_NUMBER (1000000000ULL)
-double rand_point(void) { return rand() / (double)RAND_MAX; }
-#define ROOT (0)
- 
-int rand_inside(void) {
-    double x = rand_point(), y = rand_point();
-    
-    return ( x * x + y * y ) <= 1.0;
+#include "mpi.h" 
+#include <math.h> 
+#include <time.h> 
+
+#define Iterations 10000000 //количество итераций
+
+double power(double x, double n) {
+	if (n == 0) return 1;
+	if (n < 0) return power(1.0 / x, -n);
+	return x * power(x, n - 1);
 }
- 
-int main(int argc, char ** argv) {
-    int rank, ret;
-    unsigned long long int i, hits = 0;
-    srand(time(NULL));
-    
-    ret = MPI_Init(&argc, &argv);
-    if ( ret != MPI_SUCCESS ) {
-        fprintf(stderr, "Can't init MPI!\n");
-        MPI_Abort(MPI_COMM_WORLD, ret);
-    }
-    
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if ( rank == 0 ) {
-        int numWorkers;
-        MPI_Status st;
-        double hitsSum = 0.;
-        
-        MPI_Comm_size(MPI_COMM_WORLD, &numWorkers);
-        if ( --numWorkers < 1 ) {
-            fprintf(stderr, "No workers to do job!\n");
-            MPI_Abort(MPI_COMM_WORLD, 1);
-        }
-        
-        for ( i = 0; i < numWorkers; ++i ) {
-            MPI_Recv(&hits, 1, MPI_UINT64_T, i + 1, 0, MPI_COMM_WORLD, &st);
-            hitsSum += hits;
-        }
-        
-        printf("Pi = %f\n", 4.0 * hitsSum / ((double)TEST_NUMBER * numWorkers));
-    }
-    else {
-        for ( i = 0; i < TEST_NUMBER; ++i )
-            hits += rand_inside();
-        MPI_Send(&hits, 1, MPI_UINT64_T, ROOT, 0, MPI_COMM_WORLD);
-    }
-    
-    MPI_Finalize();
-    return 0;
+
+int main() {
+
+	int ProcNum, ProcRank, i;
+	double x, MyPi = 0;
+	double Sum = 0;
+	double TotalTime;
+	clock_t StartClock, EndClock; // объявляем стартовую и конечную переменную таймера и инициализируем стартовую переменную
+	StartClock = clock();
+	MPI_Init(NULL, NULL); // инициализируем MPI
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum); // записываем количество процессов с переменную ProcNum
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank); // записываем номер процесса в переменную ProcRank
+
+	for (i = ProcRank; i < Iterations; i += ProcNum)
+	{
+		x = power(-1, ProcRank) * 4 / (2 * i + 1);
+		Sum += x;
+	}
+
+	MPI_Reduce(&Sum, &MyPi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); // пердварительные суммы Sum в переменную MyPi
+
+	MPI_Finalize(); // завершаем работу MPI
 }
